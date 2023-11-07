509c5c593961befca942f3a2bb5ed4be07246857
ptbxzrt <haozhao99@qq.com>
2023-11-07 23:18:53 +0800

    sync

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,22 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let new_str = input.trim();
+    new_str.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut new_str = input.to_string();
+    new_str.push_str(" world!");
+    new_str
 }
 
-fn replace_me(input: &str) -> String {
+fn replace_me(mut input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
